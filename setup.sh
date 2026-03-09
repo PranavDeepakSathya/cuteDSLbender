8ba3b7c48185ce2cbd691769eb8a87da060a0ced
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2026-03-09 18:37:18 +0530

    Create setup script for Python environment and packages
    Set up a Python virtual environment and install necessary packages.

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install --upgrade pip setuptools wheel
+pip install nvcc4jupyter numpy jupyter_client ipykernel ipywidgets
+pip install triton
+pip install torch
+pip install matplotlib
+pip install pandas
+pip install nvidia-cutlass-dsl
+
+
+git config --global user.email sathya.pranav.deepak@gmail.com
+git config --global user.name PranavDeepakSathya
